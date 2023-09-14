a2b5e5b2f921ebfd92bcada577b61625630e3d11
bruno.rombi@fatec.sp.gov.br <aluno@IPIRANGA.FATEC>
2023-09-14 18:03:47 -0300

    exercicio(veiculo): primeiro push

---
+++ carros.java
@@ -0,0 +1,11 @@
+public class void Carros() {
+    cor = "vermelho";
+    ano = 2020;
+    public void buzina(){
+        System.out.println("bibi!");
+    }
+    public int acelerar(velocidade){
+        acelerar+=velocidade;
+        return acelerar;
+    }
+}
