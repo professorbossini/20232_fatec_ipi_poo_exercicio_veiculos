ce15642667f306891f8d1ede29d6432bf3690838
Aluno <aluno@IPIRANGA.FATEC>
2023-09-14 18:16:05 -0300

    exercicio(veiculo): criacao da classe carros

---
+++ Carros.java
@@ -0,0 +1,20 @@
+public class Carros{
+
+    String corCarro; 
+    String placaCarro; 
+
+    //getters
+    public String getPlacaCarro(){
+        return placaCarro; 
+    } 
+
+    //setters
+    public void String setCorCarro(String corCarro){
+       this.corCarro = corCarro;  
+    }
+
+    public void exibirMensagem(){
+        System.out.printf("Bem vindo!");
+    }
+}
+
