7a0a0dea51f9b9821a6ba2966d117d21f7773775
Aluno <aluno@IPIRANGA.FATEC>
2023-09-14 18:17:33 -0300

    tamo ino

--- VeiculosMain.java
+++ VeiculosMain.java
@@ -1,4 +1,9 @@
 public class VeiculosMain{
     private String placaDoVeiculo;
-    public String
+    public String getPlacaDoVeiculo(){
+        return placaDoVeiculo;
+    }
+    public String setPlacaDoVeiculo(){
+         this.nomeDisciplina= nomeDisciplina;
+    }
 }
